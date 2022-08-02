4c9555b7a72bcb8dc3dcc74d5a520edd32f10d54
Thaysa <thay.fernnanda@gmail.com>
2022-08-02 16:49:47 -0300

    requisito 2

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,6 +8,8 @@ class Header extends Component {
     return (
       <div>
         <span data-testid="email-field">{ email }</span>
+        <span data-testid="total-field">0</span>
+        <span data-testid="header-currency-field"> BRL </span>
       </div>
     );
   }
