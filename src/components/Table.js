9602277ed1c7ecdfafba20f534f9195cfda46f38
Thaysa <thay.fernnanda@gmail.com>
2022-08-08 19:44:55 -0300

    requisito 6

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
 
-export default class Table extends Component {
+class Table extends Component {
   render() {
     return (
-      <div>Table</div>
+      <>
+        <div>Table</div>
+        <table>
+          <th>Descrição</th>
+          <th>Tag</th>
+          <th>Método de pagamento</th>
+          <th>Valor</th>
+          <th>Moeda</th>
+          <th>Câmbio utilizado</th>
+          <th>Valor convertido</th>
+          <th>Moeda de conversão</th>
+          <th>Editar/Excluir</th>
+        </table>
+      </>
     );
   }
 }
+
+export default Table;
